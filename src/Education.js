cb5ee7f0bf250070c70bdb22a182c34b21ac573c
Michael Jeffrey <michaeldjeffrey@gmail.com>
2019-01-10 18:25:24 -0700

    Convert education to a bonafide component

--- src/Education.js
+++ src/Education.js
@@ -1,4 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export const Education = () => <h1>Education</h1>;
+class Education extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            education: [],
+        };
+        this.addEducation = this.addEducation.bind(this);
+    }
+
+    addEducation(education) {
+        const joined = this.state.education.concat(education);
+        this.setState({education: joined});
+    }
+
+    render() {
+        return (
+            <div className="panel">
+              <p className="panel-heading">
+                Education
+              </p>
+              <div className="panel-block">
+                {this.state.education.map((x, index) => <EducationCard key={index} {...x}/>)}
+              </div>
+            <EducationForm submissionHandler={this.addEducation}/>
+            </div>
+        );
+    }
+}
+
 export default Education;
