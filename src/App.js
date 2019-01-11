3e8224b092a1e1e8d88a1c8e04b7e8fe04bebbc5
Michael Jeffrey <michaeldjeffrey@gmail.com>
2019-01-10 18:24:44 -0700

    Export education by default

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import 'bulma';
-import {Education} from "./Education";
+import Education from "./Education";
 import Experience from './Experience';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="App container">
                 <Education/>
                 <Experience/>
             </div>
--- src/Education.js
+++ src/Education.js
@@ -1,3 +1,4 @@
 import React from 'react';
 
 export const Education = () => <h1>Education</h1>;
+export default Education;
